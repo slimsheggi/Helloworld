9217b20ac87c8dbc71ebcb5ffb6bcf8d0eebd69d
slimsheggi <xxxfooksikxxx@gmail.com>
2024-10-21 17:00:59 +0300

    homework 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
+        //task 1
+
         byte a = 10;
         System.out.println("Значение переменной a с типом byte равно " + a);
         short b = 20;
@@ -13,6 +15,8 @@ public class Main {
         System.out.println("Значение переменной e с типом float равно " + e);
         double f = 6.0;
         System.out.println("Значение переменной f с типом double равно " + f);
+
+        //Task 2
 
         double one = 27.12;
         System.out.println(one);
@@ -29,6 +33,8 @@ public class Main {
         long seven = 987678965549L;
         System.out.println(seven);
 
+        // Task 3
+
         byte teacherL = 23;
         byte teacherA = 27;
         byte teacherE = 30;
@@ -38,6 +44,8 @@ public class Main {
         System.out.println(allPapers);
         int paperForStudents = allPapers / allStudents;
         System.out.println("На каждого ученика расчитано " + paperForStudents + " листов бумаги");
+
+        // Task 4
 
         byte bottlesProduces = 16;
         byte workingMinutes = 2;
@@ -64,6 +72,8 @@ public class Main {
         int createdBottlesMonth = (bottlesProduces / workingMinutes) * requiredOperatingTime4;
         System.out.println("За месяц машина произвела " + createdBottlesMonth + " штук бутылок");
 
+        // Task 5
+
         byte allPaint = 120;
         byte whitePaintInOneClassRum = 2;
         byte brownPaintInOneClassRum = 4;
@@ -73,6 +83,67 @@ public class Main {
         System.out.println("Всего классов " + totalClassRum);
         int totalWhitePaint = whitePaintInOneClassRum * totalClassRum;
         int totalBrownPaint = brownPaintInOneClassRum * totalClassRum;
-        System.out.println("В школе где " + totalClassRum + " классов, нужно " + totalWhitePaint + " банок белой краски и " + totalBrownPaint + " банок коричневой краски");
+        System.out.println("В школе где " + totalClassRum + " классов, нужно " + totalWhitePaint + " банок белой краски и " + totalBrownPaint + " банок коричневой краски.");
+
+        //task 6
+
+        byte totalBananas = 5;
+        short totalMilk = 200;
+        byte totalIceCream = 2;
+        byte totalEggs = 4;
+        System.out.println("Всего на завтрак " + totalBananas + " бананов, " + totalMilk + " миллилитров молока, " + totalIceCream + " мороженного и " + totalEggs + " яйца.");
+
+        byte weightOneBananas = 80;
+        short weightOneMilk = 105;
+        byte weightOneIceCream = 100;
+        byte weightOneEggs = 70;
+        System.out.println("Вес одного банана " + weightOneBananas + " грамм, вес ста миллилитров молока " + weightOneMilk + " грамм, вес одного мороженного " + weightOneIceCream + " грамм и вес одного яйца " + weightOneEggs + " грамм.");
+
+        int weightAllBananas = totalBananas * weightOneBananas;
+        int weightAllMilk = weightOneMilk + weightOneMilk;
+        int weightAllIceCream = totalIceCream * weightOneIceCream;
+        int weightAllEggs = totalEggs * weightOneEggs;
+        System.out.println("Общий вес бананов " + weightAllBananas + " грамм, молока " + weightAllMilk + " грамм, мороженного " + weightAllIceCream + " грамм, яиц " + weightAllEggs + " грамм.");
+
+        double totalBreakfastWeight = weightAllBananas + weightAllMilk + weightAllIceCream + weightAllEggs;
+        System.out.println("Общий вес завтрака " + totalBreakfastWeight + " грамм.");
+
+        double weightInKilograms = totalBreakfastWeight / 1000;
+        System.out.println("Общий вес завтрака " + weightInKilograms + " киллограмм.");
+
+        // task 7
+
+        short needToLoseAKilo = 7*1000;
+        short losesPerDay = 250;
+        short losesPerDay2 = 500;
+        int daysToLoseWeight = needToLoseAKilo / losesPerDay;
+        int daysToLoseWeight2 = needToLoseAKilo / losesPerDay2;
+        int onAverageDays = (daysToLoseWeight + daysToLoseWeight2) / 2;
+        System.out.println("Если спортсмен будет терять по " + losesPerDay + " грамм в день, ему понадобиться " + daysToLoseWeight + " дней а если по " + losesPerDay2 + " грамм в день то " + daysToLoseWeight2 + " дней. В среднем для похудения понадобится " + onAverageDays + " днь.");
+
+        // task 8
+
+        double annualIncrease = 0.1;
+
+        int salaryMari = 67760;
+        int salaryMariInYear = salaryMari * 12;
+        double salaryMariAfterPromotion = (salaryMari * annualIncrease) + salaryMari;
+        double salaryMariInYearsAfterPromotion = salaryMariAfterPromotion * 12;
+        double salaryMariDifferenceInYear = salaryMariInYearsAfterPromotion - salaryMariInYear;
+        System.out.println("Маша теперь получает " + salaryMariAfterPromotion + " рублей. Годовой доход вырос на " + salaryMariDifferenceInYear);
+
+        int salaryDen = 83690;
+        int salaryDenInYear = salaryDen * 12;
+        double salaryDenAfterPromotion = (salaryDen * annualIncrease) + salaryDen;
+        double salaryDenInYearsAfterPromotion = salaryDenAfterPromotion * 12;
+        double salaryDenDifferenceInYear = salaryDenInYearsAfterPromotion - salaryDenInYear;
+        System.out.println("Денис теперь получает " + salaryDenAfterPromotion + " рублей. Годовой доход вырос на " + salaryDenDifferenceInYear);
+
+        int salaryKris = 76230;
+        int salaryKrisInYear = salaryKris * 12;
+        double salaryKrisAfterPromotion = (salaryKris * annualIncrease) + salaryKris;
+        double salaryKrisInYearsAfterPromotion = salaryKrisAfterPromotion * 12;
+        double salaryKrisDifferenceInYear = salaryKrisInYearsAfterPromotion - salaryKrisInYear;
+        System.out.println("Кристина теперь получает " + salaryKrisAfterPromotion + " рублей. Годовой доход вырос на " + salaryKrisDifferenceInYear);
     }
 }
